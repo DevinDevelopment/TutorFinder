facb9c31e07ba192d5040f8e24e9c1c62b1cf9a5
osiris <king.forrest828@gmail.com>
2023-06-29 18:56:09 -0400

    Tutor post

--- controllers/api/tutorRoutes.js
+++ controllers/api/tutorRoutes.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 const { Tutor, Review } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+router.post('/:id', withAuth, async (req, res) => {
+  try {
+    const newReview = await Review.create({
+      ...req.body,
+      tutor_id: req.params.id,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newReview);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
+});
 router.get('/', async (req, res) => {
   try {
     const tutorData = await Tutor.findAll({
