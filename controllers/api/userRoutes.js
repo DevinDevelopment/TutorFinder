81b0a0a0a58ecf3ee98249f4c11dca7c2cb0f336
devv-nunn <devv.nunn@gmail.com>
2023-06-29 18:17:06 -0400

    checkpoint

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,33 +1,33 @@
-// const router = require('express').Router();
-// const { User } = require('../../models');
+const router = require('express').Router();
+const { User } = require('../../models');
 
-// router.get('/profile/:id', async (req, res) => {
-//   try {
-//     if (!req.session.logged_in) {
-//       res.redirect('/login');
-//       return;
-//     }
-//     const userId = req.session.user_id;
+router.get('/profile/:id', async (req, res) => {
+  try {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+      return;
+    }
+    const userId = req.session.user_id;
     
-//     const userProfile = await User.findByPk(userId, {
-//       attributes: ['id', 'username', 'email', 'description'],
-//     });
+    const userProfile = await User.findByPk(userId, {
+      attributes: ['id', 'username', 'email', 'description'],
+    });
 
-//     if (!userProfile) {
-//       res.status(404).json({ message: 'User not found' });
-//       return;
-//     }
+    if (!userProfile) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
 
-//     res.render('profile', {
-//       layout: 'main',
-//       logged_in: true,
-//       username: userProfile.username,
-//       email: userProfile.email,
-//       description: userProfile.description,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('profile', {
+      layout: 'main',
+      logged_in: true,
+      username: userProfile.username,
+      email: userProfile.email,
+      description: userProfile.description,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// module.exports = router;
+module.exports = router;
