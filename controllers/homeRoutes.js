bd692295a643eeae218ce69dfe91430696f15221
osiris <king.forrest828@gmail.com>
2023-07-04 16:54:47 -0400

    started on profile route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -87,4 +87,14 @@ router.get('/tutor/:id', async (req, res) => {
   }
 });
 
+router.get('/profile', async (req, res) => {
+  try {
+    const userId = req.session.user_id;
+    const userProfile = await User.findbyPk({ userId,
+      include: [ model:]})
+  }
+
+
+});
+
 module.exports = router;
