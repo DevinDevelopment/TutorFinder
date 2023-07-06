cc9ac63627235f83fe53cedb04ed9765f7931a78
devv-nunn <devv.nunn@gmail.com>
2023-07-05 21:29:24 -0400

    checkpoint

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const studentRoutes = require('./studentRoutes');
 const tutorRoutes = require('./tutorRoutes');
+const logoutRoute = require('./logout');
 
 router.use('/student', studentRoutes);
 router.use('/tutor', tutorRoutes);
+router.use('/logout', logoutRoute);
 
 module.exports = router;
