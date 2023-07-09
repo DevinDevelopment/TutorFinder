0a0bd1405fd48c53f78cf528815a033e922bb94e
devv-nunn <devv.nunn@gmail.com>
2023-07-09 00:52:09 -0400

    finally fixed alot of things

--- controllers/api/tutorRoutes.js
+++ controllers/api/tutorRoutes.js
@@ -1,20 +1,6 @@
 const router = require('express').Router();
 const { Tutor, Review } = require('../../models');
 const withAuth = require('../../utils/auth');
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/images'); // Set the destination folder for storing the uploaded images
-  },
-  filename: function (req, file, cb) {
-    // Generate a unique filename by adding a timestamp to the original filename
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix);
-  },
-});
-
-const upload = multer({ storage: storage });
 
 router.get('/:id', async (req, res) => {
   try {
@@ -44,28 +30,28 @@ router.post('/:id/addReview', async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       tutor_id: req.params.id,
-      user_id: req.session.user_id,
+      tutor_id: req.session.user_id,
     });
 
     res.status(200).json(newReview);
   } catch (err) {
     res.status(400).json(err);
   }
+});
 
-  // ------- Tutor update description
+// ------- Tutor update description
 
-router.put('/description', async (req, res) => {
+router.put('/tutordescription', async (req, res) => {
   try {
-    const userId = req.session.user_id;
-    const descriptionData = await User.update({ description: req.body.desc}, 
-      {where: {id : userId}}
+    const tutorId = req.session.tutor_id;
+    console.log(tutorId);
+    const descriptionData = await Tutor.update({ description: req.body.tutorDesc}, 
+      {where: {id : tutorId}}
       );
-
     res.status(200).json(descriptionData);
   } catch (err) {
     res.status(400).json(err);
   }
-});
 });
 
 module.exports = router;
